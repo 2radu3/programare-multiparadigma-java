5f8be6bbcf51022f2d01decdd4f6aac980360ac3
2radu3 <2radupmihai3@gmail.com>
2026-03-25 18:51:58 +0200

    seminar_06

--- src/seminar/seminar_05/src/Main.java
+++ src/seminar/seminar_05/src/Main.java
@@ -1,12 +1,16 @@
 package seminar.seminar_05.src;
 
+import seminar.seminar2.g1061.Categorie;
+import seminar.seminar2.g1061.ElementPatrimonial;
 import seminar.seminar2.g1061.MijlocFix;
 import seminar.seminar2.g1061.Locatie;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -64,6 +68,111 @@ public class Main {
             List<MijlocFix> cerinta3 = lista.stream().filter(mijlocFix -> mijlocFix.getDataAchizitie().before(dataLimita) ).toList();
             System.out.println(("Mijloace fixe achizitionate inainte de: " + seminar.seminar2.g1061.Main.fmt.format(dataLimita) + ":"));
             cerinta3.forEach(System.out::println);
+
+            // Seminar 6
+            // Cerinta 4
+//            List<MijlocFix> cerinta4 = lista.stream().sorted(new Comparator<MijlocFix>() {
+//                @Override
+//                public int compare(MijlocFix o1, MijlocFix o2) {
+//                    return Double.compare(o1.calculUzura(),o2.calculUzura());
+//                }
+//            }).toList();
+            List<MijlocFix> cerinta4 = lista.stream().sorted((o1,o2)->Double.compare(o1.calculUzura(),o2.calculUzura())).toList(); //sau asa
+            System.out.println("Lista sortata dupa uzura");
+            cerinta4.forEach(mijlocFix -> System.out.println(mijlocFix.getDenumire()+","+mijlocFix.calculUzura()));
+
+            // Cerinta 5
+            Set<String> cerinta5 = lista.stream().map(mijlocFix -> mijlocFix.getLocatie().getDenumire()).collect(Collectors.toSet());
+            System.out.println("\nLista locatiilor: ");
+            cerinta5.forEach(System.out::println);
+
+            // Cerinta 6
+            Map<Long, Locatie> cerinta6 = lista.stream().collect(Collectors.toMap(MijlocFix::getNrInventar, MijlocFix::getLocatie));
+            System.out.println("\nColectare locatii pe numere de inventar: ");
+            cerinta6.forEach((nrinv,locatie)-> System.out.println(nrinv+","+locatie));
+
+            // Cerinta 7
+            Map<Categorie, List<MijlocFix>> cerinta7 = lista.stream().collect(Collectors.groupingBy(MijlocFix::getCategorie));
+            System.out.println("\nGrupare mijloace fixe pe categorii");
+            cerinta7.forEach(((categorie, mijloaceFixe) -> {
+                System.out.println("Categorie: "+ categorie);
+                mijloaceFixe.forEach(System.out::println);
+            }));
+
+            // Cerinta 8
+            Map<Categorie, List<String>> cerinta8 = lista.stream().collect(Collectors.groupingBy(MijlocFix::getCategorie,Collectors.mapping(MijlocFix::getDenumire,Collectors.toList())));
+            System.out.println("\nGrupare mijloace fixe pe categorii: ");
+            cerinta8.forEach(((categorie,denumiri)-> System.out.println(categorie+","+denumiri)));
+
+            // Cerinta 9
+            Map <String, Double> cerinta9 = lista.stream().collect(Collectors.groupingBy(mijlocFix -> mijlocFix.getLocatie().getDenumire(), Collectors.averagingDouble(MijlocFix::calculUzura)));
+            System.out.println("\nUzura medie pe locatii:");
+            cerinta9.forEach((denumirel,uzura)-> System.out.println(denumirel+","+uzura));
+
+//            class Info{
+//                //si punem aici alea de mai jos
+//            }
+
+            // Cerinta 10
+            Map<Long,?> cerinta10 = lista.stream()
+                    .collect(Collectors.toMap(
+                            MijlocFix::getNrInventar,
+                            mijlocFix -> new Object(){
+                                private String denumire = mijlocFix.getDenumire();
+                                private Date dataA = mijlocFix.getDataAchizitie();
+
+                                @Override
+                                public String toString() {
+                                    return denumire+","+(dataA==null?"": seminar.seminar2.g1061.Main.fmt.format(dataA));
+                                }
+                            }
+                    ));
+            System.out.println("\nColectare informatii pe mijloc fix (numar inventar)");
+            cerinta10.forEach((nrinv,info)-> System.out.println(nrinv+":"+info));
+
+            // Cerinta 11
+//            Map<Categorie,List<Long>> cerinta11 = lista.stream()
+//                    .collect(new Supplier<Map<Categorie, List<Long>>>() {
+//                        @Override
+//                        public Map<Categorie, List<Long>> get() {
+//                            return new HashMap<>();
+//                        }
+//                    }, new BiConsumer<Map<Categorie, List<Long>>, MijlocFix>() {
+//                        @Override
+//                        public void accept(Map<Categorie, List<Long>> map, MijlocFix mijlocFix) {
+//                            Categorie categorie = mijlocFix.getCategorie();
+//                            if (map.containsKey(categorie)){
+//                                map.get(categorie).add(mijlocFix.getNrInventar());
+//                            } else {
+//                                List<Long> numereInventar = new ArrayList<>();
+//                                numereInventar.add(mijlocFix.getNrInventar());
+//                                map.put(categorie,numereInventar);
+//                            }
+//                        }
+//                    }, new BiConsumer<Map<Categorie, List<Long>>, Map<Categorie, List<Long>>>() {
+//                        @Override
+//                        public void accept(Map<Categorie, List<Long>> map1, Map<Categorie, List<Long>> map2) {
+//                            map1.putAll(map2);
+//                        }
+//                    });
+            Map<Categorie, List<Long>> cerinta11 = lista.stream()
+                    .collect(
+                            HashMap::new,
+                            (map, mijlocFix) -> {
+                                Categorie categorie = mijlocFix.getCategorie();
+                                if (map.containsKey(categorie)) {
+                                    map.get(categorie).add(mijlocFix.getNrInventar());
+                                } else {
+                                    List<Long> numereInventar = new ArrayList<>();
+                                    numereInventar.add(mijlocFix.getNrInventar());
+                                    map.put(categorie, numereInventar);
+                                }
+                            },
+                            HashMap::putAll
+                    );
+            System.out.println("\nColectare numere inventar pe categorii:");
+            cerinta11.forEach(((categorie, numere) -> System.out.println(categorie + ":" + numere)));
+
 
 
 
